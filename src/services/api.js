c5423abdfe0e8651ec15f9586649267f678629bd
pilla20202020 <bsbishalpilla@gmail.com>
2025-07-01 23:10:34 +0545

    base URL changed for qa

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // update if needed
+  // baseURL: 'http://localhost:8000/api', // update if needed
+  baseURL: 'https://test.manishadhikari7.com.np/api', // update if needed
 })
 
 export default api
